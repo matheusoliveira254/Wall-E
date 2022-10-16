f30d942175735d177876b095310785a9cd50a06b
Matheus Oliveira <matheus.oliveira254@gmail.com>
2022-10-15 18:02:44 -0600

    created the storyboards

---
+++ Wall-E/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wall-E
+//
+//  Created by Matheus Oliveira on 10/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Wall-E/Model/Photos.swift
@@ -0,0 +1,8 @@
+//
+//  Photos.swift
+//  Wall-E
+//
+//  Created by Matheus Oliveira on 10/15/22.
+//
+
+import Foundation
